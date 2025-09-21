5a0a74ee70835b0ea8c0578b6d15d9a86cb7d11a
Mittelblut9 <sauer.benedikt@outlook.de>
2025-09-21 23:06:28 +0200

    feat: add index file to export interfaces and enums

---
+++ lib/index.ts
@@ -0,0 +1,13 @@
+export * from './interfaces/IAppwrite';
+export * from './interfaces/IGeneral';
+export * from './interfaces/IModel';
+export * from './interfaces/IOBS';
+export * from './interfaces/IPermissions';
+export * from './interfaces/IScreenElements';
+export * from './interfaces/ISpotify';
+export * from './interfaces/IStreamElements';
+
+// enums
+export * from './enum/appwrite.enum';
+export * from './enum/default.enum';
+export * from './enum/enum';
